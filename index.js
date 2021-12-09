ab75fca4b37157ff507d8652ebfdb6ff5e2b0d9d
henrique.schmitt <henrique.schmitt@mundiale.com.br>
2021-12-09 17:53:30 -0300

    feat: methods are now commented at index

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ require('dotenv').config({ path: 'env/.env' })
 
 const AwsController = require('./src/controllers/aws-controller')
 
-AwsController.listBuckets()
+// AwsController.listBuckets()
+// AwsController.listObject()
+// AwsController.put()
+// AwsController.list()
